9cadd78363fc2b21c7e7b6e10984d9517a9fae45
mohamad <mohammad-hamieh@hotmail.com>
2017-11-28 13:22:18 +0200

    cart api

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -15,7 +15,7 @@ class CartController extends Controller
      */
 
 //{"data":[{"Id":11,"qty":1},{"Id":13,"qty":4}]}
-    public function get_cart($data,$user)
+    public function get_cart($data,$userid)
     {
         $user = json_decode($data);
          try{
@@ -27,7 +27,7 @@ class CartController extends Controller
          $cart->json_cart = "a";
           $cart->qty = $mydata->qty;
            $cart->idoffer = $mydata->Id;
-            $cart->iduser = $user;
+            $cart->iduser = $userid;
            $cart->save();
     }   
     return "[{".'"status":'.'"Uploaded Successfully"'."}]";   
