22ec42c498bc7e77e5ff24ef2b27f013b3f4274d
mohamad <mohammad-hamieh@hotmail.com>
2017-12-07 18:05:03 +0200

    cart controller

--- resources/views/admin/offers/cart_offer_buy_spec.blade.php
+++ resources/views/admin/offers/cart_offer_buy_spec.blade.php
@@ -5,11 +5,14 @@
     <div class="row">
         <div class="col-md-12">
             <div class="panel panel-default">
-                <div class="panel-heading text-center" style="color: red;font-weight: 900"><!-- Username : {{$carts_offer[0]->iduser}} -->
+                <div class="panel-heading text-center" style="color: red;font-weight: 900">Username : {{$carts_offer[0]->iduser}}
 <span class="pull-right">Total: {{$thetotalall}} L.L</span></div>
 
                <table style="text-align: center;border: 1px solid #ddd!important">
+                @if(count($carts_offer)==0)
+                @else
                     @foreach($carts_offer as $cart)
+                
   
        <tr border: 1px solid #ddd!important>
         <td style="height:50px;width:50px; background: url('{{asset('uploads/offers/'.$cart->offer->img_name)}}'); background-size: 100%; background-position: center center;border: 1px solid #ddd!important"></td>
@@ -19,6 +22,9 @@
         <td style="width:350px;border: 1px solid #ddd!important"><?php echo $cart->qty * $cart->offer->price;  ?> L.L</td>
   
     @endforeach
+    @endif
+       @if(count($carts_item)==0)
+                @else
      @foreach($carts_item as $cartitem)
   
        <tr border: 1px solid #ddd!important>
@@ -29,6 +35,9 @@
         <td style="width:350px;border: 1px solid #ddd!important"><?php echo $cartitem->qty * $cartitem->item->price;  ?> L.L</td>
   
     @endforeach
+  @endif
+    @if(count($carts_sub_item)==0)
+                @else
      @foreach($carts_sub_item as $cartchild)
   
        <tr border: 1px solid #ddd!important>
@@ -39,7 +48,7 @@
         <td style="width:350px;border: 1px solid #ddd!important"><?php echo $cartchild->qty * $cartchild->child->price;  ?> L.L</td>
   
     @endforeach
-
+  @endif
 </table>
 
   
