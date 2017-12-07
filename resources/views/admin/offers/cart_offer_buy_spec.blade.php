367e5a7ad02b5a6ea23082343074bbb101539b0e
mohamad <mohammad-hamieh@hotmail.com>
2017-12-07 18:12:21 +0200

    cart controller

--- resources/views/admin/offers/cart_offer_buy_spec.blade.php
+++ resources/views/admin/offers/cart_offer_buy_spec.blade.php
@@ -6,6 +6,9 @@
         <div class="col-md-12">
             <div class="panel panel-default">
                 <div class="panel-heading text-center" style="color: red;font-weight: 900">
+
+
+                    Username : ABCDEF
 <span class="pull-right">Total: {{$thetotalall}} L.L</span></div>
 
                <table style="text-align: center;border: 1px solid #ddd!important">
