7da1ebc900af18fb040d5cabc733a78c326d1da5
mohamad <mohammad-hamieh@hotmail.com>
2017-12-07 18:19:46 +0200

    cart controller

--- resources/views/admin/offers/cart_offer_buy_spec.blade.php
+++ resources/views/admin/offers/cart_offer_buy_spec.blade.php
@@ -18,7 +18,7 @@
                 
   
        <tr border: 1px solid #ddd!important>
-        <td style="height:100px;width:100px; background: url('{{asset('uploads/offers/'.$cart->offer->img_name)}}'); background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
+        <td style="height:100px;width:100px; background: url('{{asset('uploads/offers/'.$cart->offer->img_name)}}'); background-repeat: no-repeat;background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
         <td style="width:300px;border: 1px solid #ddd!important">{{ $cart->offer->title }}</td>
         <td style="width:200px;border: 1px solid #ddd!important">{{$cart->qty}}</td>
         <td style="width:350px;border: 1px solid #ddd!important">{{$cart->offer->price}} L.L</td>
@@ -31,7 +31,7 @@
      @foreach($carts_item as $cartitem)
   
        <tr border: 1px solid #ddd!important>
-        <td style="height:100px;width:100px; background: url('{{asset('uploads/items/'.$cartitem->item->img_name)}}'); background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
+        <td style="height:100px;width:100px; background: url('{{asset('uploads/items/'.$cartitem->item->img_name)}}');background-repeat: no-repeat; background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
         <td style="width:300px;border: 1px solid #ddd!important">{{ $cartitem->item->name }}</td>
         <td style="width:200px;border: 1px solid #ddd!important">{{$cartitem->qty}}</td>
         <td style="width:350px;border: 1px solid #ddd!important">{{$cartitem->item->price}} L.L</td>
@@ -44,7 +44,7 @@
      @foreach($carts_sub_item as $cartchild)
   
        <tr border: 1px solid #ddd!important>
-        <td style="height:100px;width:100px; background: url('{{asset('uploads/items/childs/'.$cartchild->child->img_name)}}'); background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
+        <td style="height:100px;width:100px; background: url('{{asset('uploads/items/childs/'.$cartchild->child->img_name)}}'); background-repeat: no-repeat;background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
         <td style="width:300px;border: 1px solid #ddd!important">{{ $cartchild->child->name_sub }}</td>
         <td style="width:200px;border: 1px solid #ddd!important">{{$cartchild->qty}}</td>
         <td style="width:350px;border: 1px solid #ddd!important">{{$cartchild->child->price}} L.L</td>
