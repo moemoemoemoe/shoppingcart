13b8d1a2180bf6b2e0395c0101bd4065506bee11
mohamad <mohammad-hamieh@hotmail.com>
2017-12-14 12:13:05 +0200

    cart

--- resources/views/admin/offers/cart_offer_buy.blade.php
+++ resources/views/admin/offers/cart_offer_buy.blade.php
@@ -9,7 +9,7 @@
 
                 <div class="panel-body">
                    @foreach($carts as $cart)
-                   <a href="{!! route('view_cart_offer_spec', ['invm'=>$cart->original_invoice]) !!}"><span class="btn btn-primary">{{$cart->original_invoice}}</span></a>
+                   <a href="{!! route('view_cart_offer_spec', ['invm'=>$cart->original_invoice]) !!}"><span class="btn btn-primary" style="margin: 10px">{{$cart->original_invoice}}</span></a>
                    @endforeach
 
                 </div>
