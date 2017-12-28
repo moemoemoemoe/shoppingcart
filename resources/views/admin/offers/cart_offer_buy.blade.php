b75de2b48602f426b43b012ab12f97cc15c289ae
mohamad <mohammad-hamieh@hotmail.com>
2017-12-28 13:31:44 +0200

    order

--- resources/views/admin/offers/cart_offer_buy.blade.php
+++ resources/views/admin/offers/cart_offer_buy.blade.php
@@ -10,10 +10,15 @@
                 <div class="panel-body">
                    @foreach($carts as $cart)
                   <div class="col-md-3"> <a href="{!! route('view_cart_offer_spec', ['invm'=>$cart->inv_id]) !!}"><span class="btn btn-primary" style="margin: 10px">{{$cart->inv_id}}</span></a></div>
-                  <div class="col-md-3"><span style="margin: 10px;font-weight: 900">Driver : {{$cart->driver->name}}</span></div>
-                <div class="col-md-3"><span  style="margin: 10px;font-weight: 900">Role : {{$cart->role}}</span></div>
-                <div class="col-md-3"><span  style="margin: 10px;font-weight: 900;color: red">{{$cart->created_at}}</span></div>
-
+                  @if($cart->role =='X')
+                  <div class="col-md-3"><span style="margin: 10px;font-weight: 900;color: red">Not Assigned</span></div>
+                  <div class="col-md-3"><span  style="margin: 10px;font-weight: 900;color: red"> No Set</span></div>
+                  <div class="col-md-3"><span  style="margin: 10px;font-weight: 900;color: red">{{$cart->created_at}}</span></div>
+                  @else
+                  <div class="col-md-3"><span style="margin: 10px;font-weight: 900"> {{$cart->driver->name}}</span></div>
+                  <div class="col-md-3"><span  style="margin: 10px;font-weight: 900"> {{$cart->role}}</span></div>
+                  <div class="col-md-3"><span  style="margin: 10px;font-weight: 900;color: red">{{$cart->created_at}}</span></div>
+@endif
 
                    @endforeach
 
