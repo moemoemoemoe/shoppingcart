c08a7516e893cbf926f913e7b8a09fa36c4dd673
mohamad <mohammad-hamieh@hotmail.com>
2017-12-28 13:17:38 +0200

    order

--- app/Order.php
+++ app/Order.php
@@ -7,6 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
      public function driver(){
-    	return $this->belongsTo('App\Driver');
+    	return $this->belongsTo('App\Driver','driver_id');
     }
 }
