39dbe9e7471647378f7e7721d14ea3b7a93a714e
mohamad <mohammad-hamieh@hotmail.com>
2017-12-28 13:29:09 +0200

    order

--- resources/views/admin/offers/cart_offer_buy.blade.php
+++ resources/views/admin/offers/cart_offer_buy.blade.php
@@ -10,9 +10,9 @@
                 <div class="panel-body">
                    @foreach($carts as $cart)
                   <div class="col-md-3"> <a href="{!! route('view_cart_offer_spec', ['invm'=>$cart->inv_id]) !!}"><span class="btn btn-primary" style="margin: 10px">{{$cart->inv_id}}</span></a></div>
-                  <div class="col-md-3"><span class="btn btn-primary" style="margin: 10px">{{$cart->driver->name}}</span></div>
-                <div class="col-md-3"><span class="btn btn-primary" style="margin: 10px">{{$cart->driver->role}}</span></div>
-                <div class="col-md-3"><span class="btn btn-primary" style="margin: 10px">{{$cart->created_at}}</span></div>
+                  <div class="col-md-3"><span style="margin: 10px;font-weight: 900">Driver : {{$cart->driver->name}}</span></div>
+                <div class="col-md-3"><span  style="margin: 10px;font-weight: 900">Role : {{$cart->role}}</span></div>
+                <div class="col-md-3"><span  style="margin: 10px;font-weight: 900;color: red">{{$cart->created_at}}</span></div>
 
 
                    @endforeach
