7aeedb0964a32126a408cf6715f20dd14e706adc
mohamad <mohammad-hamieh@hotmail.com>
2017-12-28 13:33:21 +0200

    order

--- resources/views/admin/offers/cart_offer_buy.blade.php
+++ resources/views/admin/offers/cart_offer_buy.blade.php
@@ -7,8 +7,9 @@
             <div class="panel panel-default">
                 <div class="panel-heading">View Invoices</div>
 
-                <div class="panel-body">
+              
                    @foreach($carts as $cart)
+                     <div class="panel-body">
                   <div class="col-md-3"> <a href="{!! route('view_cart_offer_spec', ['invm'=>$cart->inv_id]) !!}"><span class="btn btn-primary" style="margin: 10px">{{$cart->inv_id}}</span></a></div>
                   @if($cart->role =='X')
                   <div class="col-md-3"><span style="margin: 10px;font-weight: 900;color: red">Not Assigned</span></div>
@@ -19,10 +20,10 @@
                   <div class="col-md-3"><span  style="margin: 10px;font-weight: 900"> {{$cart->role}}</span></div>
                   <div class="col-md-3"><span  style="margin: 10px;font-weight: 900;color: red">{{$cart->created_at}}</span></div>
 @endif
-
+</div>
                    @endforeach
 
-                </div>
+                
             </div>
         </div>
     </div>
