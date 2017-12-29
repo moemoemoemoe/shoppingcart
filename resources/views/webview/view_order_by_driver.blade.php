86e8cc4c72dc62c67526c2fbeb5eed7c099dce6a
mohamad <mohammad-hamieh@hotmail.com>
2017-12-29 15:37:14 +0200

    order

--- resources/views/webview/view_order_by_driver.blade.php
+++ resources/views/webview/view_order_by_driver.blade.php
@@ -61,7 +61,7 @@
             <a  type="button" class="btn-floating btn-small btn-danger" style="background-color: red"><i class="fa fa-check"></i></a>
 
     
-        <!-- <a href="{!! route('confirm_order', ['id'=>$order->id]) !!}"  type="button" class="btn-floating btn-small btn-tw"><i class="fa fa-ban"></i></a> -->
+      
         <a " type="button" class="btn-floating btn-small btn-dribbble"><i class="fa fa-map"></i></a>
         <a  type="button" class="btn-floating btn-small btn-primary" style="background-color: green"><i class="fa fa-eye"></i></a>
 
