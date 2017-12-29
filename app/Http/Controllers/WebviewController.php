4c94e3b4a529cafd96d41008c4cc4b2e23f13927
mohamad <mohammad-hamieh@hotmail.com>
2017-12-29 10:23:15 +0200

    order

--- app/Http/Controllers/WebviewController.php
+++ app/Http/Controllers/WebviewController.php
@@ -18,7 +18,9 @@ class WebviewController extends Controller
     public function orders($email)
     {
         $orders = Order::orderBy('id','DESC')->with('driver')->get();
-        return $orders;
+        //return $orders;
+        $date = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $orders[0]->created_at)->format('Y-m-d');
+        return $date;
 return view('webview.orders');
         
     }
