4490e0cedfb6bfc1a5dd39b586e6d5726572b8aa
mohamad <mohammad-hamieh@hotmail.com>
2018-01-02 12:06:19 +0200

    webview

--- app/Http/Controllers/WebviewController.php
+++ app/Http/Controllers/WebviewController.php
@@ -30,7 +30,7 @@ class WebviewController extends Controller
        //return $orders;
         // $date = \Carbon\Carbon::createFromFormat('Y-m-d H:i:s', $orders[0]->created_at)->format('Y-m-d');
         // return $date.' today is : '.date('Y-m-d');
-return view('webview.orders',compact('orders','orders_pending'));
+return view('webview.orders',compact('orders','counts'));
         
     }
 
