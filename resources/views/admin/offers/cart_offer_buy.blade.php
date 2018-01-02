36d53c5d264c076d224040a2dbfa428f3e331136
mohamad <mohammad-hamieh@hotmail.com>
2018-01-02 11:26:38 +0200

    webview

--- resources/views/admin/offers/cart_offer_buy.blade.php
+++ resources/views/admin/offers/cart_offer_buy.blade.php
@@ -26,9 +26,9 @@
   @elseif($cart->status == 2)
   <div class="col-md-2"><span  style="margin: 10px;font-weight: 900;color: green">Assigned Verify </span></div>
                   @elseif($cart->status == 3)
-  <div class="col-md-2"><span  style="margin: 10px;font-weight: 900;color: red"> Finish shop </span></div>
+  <div class="col-md-2"><span  style="margin: 10px;font-weight: 900;color: red"> Rejected </span></div>
    @elseif($cart->status ==4)
-  <div class="col-md-2"><span  style="margin: 10px;font-weight: 900;color: red"> Delevired </span></div>
+  <div class="col-md-2"><span  style="margin: 10px;font-weight: 900;color: red"> Finish shop  </span></div>
   @endif
 
                   <div class="col-md-3"><span  style="margin: 10px;font-weight: 900;color: red">{{$cart->created_at}}</span></div>
