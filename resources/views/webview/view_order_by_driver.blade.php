315f489fc3115ba09e53129cdd99dc41e9f96f18
mohamad <mohammad-hamieh@hotmail.com>
2018-01-02 13:38:51 +0200

    webview

--- resources/views/webview/view_order_by_driver.blade.php
+++ resources/views/webview/view_order_by_driver.blade.php
@@ -269,8 +269,10 @@
 <script type="text/javascript">
     function show_image(url)
     {
-   var src = document.getElementById("imagepreview");
-   src.appendChild(url);
+        window.alert(url);
+        document.getElementById("imagepreview").src=url;
+   // var src = document.getElementById("imagepreview");
+   // src.appendChild(url);
    //$('#imagepreview').attr(); // here asign the image to the modal when the user click the enlarge link
    $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
 }
