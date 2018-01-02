5fbde347aa9b7cfb249c8e2c96692529aac05d9c
mohamad <mohammad-hamieh@hotmail.com>
2018-01-02 11:54:21 +0200

    webview

--- resources/views/webview/view_order_by_driver.blade.php
+++ resources/views/webview/view_order_by_driver.blade.php
@@ -115,11 +115,15 @@
     
       
         <!-- <a type="button" class="btn-floating btn-small btn-dribbble"><i class="fa fa-map"></i></a> -->
-        @if($cart->status == 0 && $ready != 2)
+        @if($ready !=4)
+
+        @if($cart->status == 0)
         <a href="{!! route('check_inv', ['id'=>$cart->id]) !!}"  type="button" class="btn-floating btn-small btn-primary" style="background-color: green"><i class="fa fa-check"></i></a>
         @else
                 <a href="{!! route('check_inv', ['id'=>$cart->id]) !!}"  type="button" class="btn-floating btn-small btn-primary" style="background-color: red"><i class="fa fa-check"></i></a>
 
+        @endif
+        @else
         @endif
 
     </div>
