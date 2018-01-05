8a4b61fc6c9b526299eab22a23fed8d9cdb0d3bc
mohamad <mohammad-hamieh@hotmail.com>
2018-01-05 13:13:44 +0200

    cart

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -19,7 +19,7 @@ class CartController extends Controller
 //{"data":[{"Id":11,"qty":1},{"Id":13,"qty":4}]}
     public function get_cart($data,$userid,$em,$ad,$phone,$tab,$x,$y,$date,$time,$cmnt,$regid)
     { 
-     // return strlen($data);
+      return strlen($data);
     $inv_last = Cart::OrderBy('id','DESC')->limit(1)->get();
     
  $carts = Cart::orderBy('id','DESC')->where('email',$em)->limit(1)->get();
