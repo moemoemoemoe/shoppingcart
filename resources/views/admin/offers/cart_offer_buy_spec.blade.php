810e2785f726bed73cc154c2335c65395b1e3137
mohamad <mohammad-hamieh@hotmail.com>
2018-02-01 11:44:09 +0200

    delete

--- resources/views/admin/offers/cart_offer_buy_spec.blade.php
+++ resources/views/admin/offers/cart_offer_buy_spec.blade.php
@@ -55,15 +55,16 @@
                <table style="text-align: center;border: 1px solid #ddd!important">
                 @if(count($carts_offer)==0)
                 @else
-                    @foreach($carts_offer as $cart)
+                     @foreach($carts_offer as $cart)
                 
   
        <tr border: 1px solid #ddd!important>
         <td style="height:100px;width:100px; background: url('{{asset('uploads/offers/'.$cart->offer->img_name)}}'); background-repeat: no-repeat;background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
         <td style="width:300px;border: 1px solid #ddd!important">{{ $cart->offer->title }}</td>
-        <td style="width:200px;border: 1px solid #ddd!important">{{$cart->qty}}</td>
-        <td style="width:350px;border: 1px solid #ddd!important">{{$cart->offer->price}} L.L</td>
-        <td style="width:350px;border: 1px solid #ddd!important"><?php echo $cart->qty * $cart->offer->price;  ?> L.L</td>
+       <td style="width:200px;border: 1px solid #ddd!important"><center><input type="number" id="qty_{{$cart->id}}" value="{{$cart->qty}}" class="form-control text-center" style="width: 100px;" /></center></td>
+        <td style="width:200px;border: 1px solid #ddd!important">{{$cart->offer->price}} L.L</td>
+        <td style="width:200px;border: 1px solid #ddd!important"><?php echo $cart->qty * $cart->offer->price;  ?> L.L</td>
+         <td style="width:250px;border: 1px solid #ddd!important"><button class="btn btn-primary" onclick="update_qty({{$cart->id}},{{$cart->original_invoice}})">Update Qty</button></a></td>
   
     @endforeach
     @endif
@@ -82,14 +83,15 @@
   @endif
     @if(count($carts_sub_item)==0)
                 @else
-     @foreach($carts_sub_item as $cartchild)
+    @foreach($carts_sub_item as $cartchild)
   
        <tr border: 1px solid #ddd!important>
         <td style="height:100px;width:100px; background: url('{{asset('uploads/items/childs/'.$cartchild->child->img_name)}}'); background-repeat: no-repeat;background-size: contain; background-position: center center;border: 1px solid #ddd!important"></td>
         <td style="width:300px;border: 1px solid #ddd!important">{{ $cartchild->child->name_sub }}</td>
-        <td style="width:200px;border: 1px solid #ddd!important">{{$cartchild->qty}}</td>
-        <td style="width:350px;border: 1px solid #ddd!important">{{$cartchild->child->price}} L.L</td>
-        <td style="width:350px;border: 1px solid #ddd!important"><?php echo $cartchild->qty * $cartchild->child->price;  ?> L.L</td>
+        <td style="width:200px;border: 1px solid #ddd!important"><center><input type="number" id="qty_{{$cartchild->id}}" value="{{$cartchild->qty}}" class="form-control text-center" style="width: 100px;" /></center></td>
+        <td style="width:200px;border: 1px solid #ddd!important">{{$cartchild->child->price}} L.L</td>
+        <td style="width:200px;border: 1px solid #ddd!important"><?php echo $cartchild->qty * $cartchild->child->price;  ?> L.L</td>
+       <td style="width:250px;border: 1px solid #ddd!important"><button class="btn btn-primary" onclick="update_qty({{$cartchild->id}},{{$cartchild->original_invoice}})">Update Qty</button></a></td>
   
     @endforeach
   @endif
